e1948d937da874d655591d2e319559f0e37c7942
Jerryt-Theronier <jerrytheronier@gmail.com>
2023-10-16 22:06:25 -0400

    Routes + SUID

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite';
 import solid from 'vite-plugin-solid';
 import * as path from 'path';
+import solidPlugin from 'vite-plugin-solid';
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), solidPlugin()],
+  build: {
+    target: 'esnext',
+  },
   server: {
     port: 3000,
   },
