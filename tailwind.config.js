2331a5b40c67c2e476b56de01d3a0610726c7d08
Jerryt-Theronier <jerrytheronier@gmail.com>
2023-10-16 21:10:03 -0400

    linting + tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.{html,scss}', './src/**/*.{tsx,scss}'],
+  important: '#root',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
